21125bdb47ecdd6df783cf2c6694bd2730f30358
martin sereinig <ms@veryunited.net>
2024-08-24 23:18:27 +0200

    first working proper minification

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 ## 1kb
 
 just a small website.
+
+### build
+
+0. use something like pyenv to have the recommended `.python-version` installed
+1. `python -m venv venv`
+2. `source venv/bin/activate`
+3. `pip install -r requirements.txt`
+4. upon calling `build.py`, every `.html` file in `src` will be minified
---
+++ build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+
+import minify_html
+
+SRC_DIR = "src"
+BUILD_DIR = "build"
+
+print("Building...")
+for filename in os.listdir(SRC_DIR):
+    with open(os.path.join(SRC_DIR, filename)) as f:
+        if not filename.endswith(".html"):
+            continue
+        minified = minify_html.minify(
+            f.read(), minify_js=True, remove_processing_instructions=True
+        )
+    with open(os.path.join(BUILD_DIR, filename), "w") as f:
+        f.write(minified)
+
+print("Done!")
